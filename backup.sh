1c3cdf15d93a23fd0731bdb02b766fbc7d909f04
David Meixner <dpmeixner@gmail.com>
2024-08-17 14:14:34 -0400

    Add timestamp to veirfy daily backups of Pictures and Documents

--- backup.sh
+++ backup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# This script will backup the Documents folder. Paired with a launchd process, it will run at midnight every night, or whenever the computer next starts up. It does not sync the directory every time a file is changed, but that is something to look into.
+# This script will backup the Documents folder. Paired with a launchd process, it will run at 
+# midnight every night, or whenever the computer next starts up.
 if ! pgrep -x rsync > /dev/null ; then
-    rsync -hvrltD --modify-window=1 --delete --log-file=~/backup.log ~/Documents /Volumes/G_DRIVE/MacBook_Air
+    touch '/Users/davidmeixner/Documents/.timestamp'
+    touch '/Users/davidmeixner/Pictures/.timestamp'
+    echo "Start:" `date` > /Users/davidmeixner/backup.log
+    rsync -hvrltD --modify-window=1 --delete-during "/Users/davidmeixner/Documents" "/Volumes/G_DRIVE/MacBook_Air"
+    rsync -a "/Users/davidmeixner/Pictures" "/Volumes/G_DRIVE"
+    echo "  End:" `date` >> /Users/davidmeixner/backup.log
 fi
